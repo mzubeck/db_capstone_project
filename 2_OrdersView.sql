51a01c5ca13fe102693678531ced85fe38f71094
mzubeck <106111888+mzubeck@users.noreply.github.com>
2023-05-18 09:57:36 -0500

    Step 2 update

--- 2_OrdersView.sql
+++ 2_OrdersView.sql
@@ -1,6 +1,9 @@
-# CREATE VIEW OrdersView AS
-# SELECT OrderID, Quantity, Cost
-#FROM Orders
-#WHERE Quantity >2;
+#create view
+/*
+CREATE VIEW OrdersView AS
+SELECT OrderID, Quantity, Cost
+FROM Orders
+WHERE Quantity >2;
+*/
 
 SELECT * FROM OrdersView;
