f7b8f532d3dd6e27a091adb26f34cc2269aa73e3
mzubeck <106111888+mzubeck@users.noreply.github.com>
2023-05-18 09:59:51 -0500

    Step 7 update

--- 7_CancelOrder.sql
+++ 7_CancelOrder.sql
@@ -1,21 +1,23 @@
-#CREATE TABLE Confirmation (Confirmation VARCHAR(60));
+/*
+CREATE TABLE Confirmation (Confirmation VARCHAR(60));
 
-#CREATE TRIGGER AfterDeleteOrder
-#	AFTER DELETE
-#	ON Orders FOR EACH ROW
-#INSERT INTO Confirmation VALUES (CONCAT('Order ', OLD.OrderID, ' is cancelled.'));
+CREATE TRIGGER AfterDeleteOrder
+	AFTER DELETE
+	ON Orders FOR EACH ROW
+INSERT INTO Confirmation VALUES (CONCAT('Order ', OLD.OrderID, ' is cancelled.'));
 
-#CREATE PROCEDURE CancelOrder (IN order_id INT)
-#DELETE FROM Orders WHERE OrderID = order_id;
+CREATE PROCEDURE CancelOrder (IN order_id INT)
+DELETE FROM Orders WHERE OrderID = order_id;
 
 #OrderID 5 is present
-#SELECT * FROM Orders;
+SELECT * FROM Orders;
 
-#CALL CancelOrder(5);
+*/
 
 #OrderID 5 is NOT present
+#CALL CancelOrder(5);
 #SELECT * FROM Orders;
 
 #Put OrderID 5 back in
 #INSERT INTO Orders (OrderID, CustomerID, StaffID, MenuID, BookingID, Date, Quantity, Cost) VALUES (5, 1, 3, 8, NULL, '2021-11-11', 5, 450);
-
+#SELECT * FROM Orders;
